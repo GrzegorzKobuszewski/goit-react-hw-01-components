26f5e72e19ec7143764390a7e5ef9e31104698b3
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-10-13 03:27:49 +0200

    Task-01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+
+import user from './components/Task-01-Profile/user.json';
+
+import { App } from 'components/App';
+import { Profile } from './components/Task-01-Profile/Profile';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
   </React.StrictMode>
 );
