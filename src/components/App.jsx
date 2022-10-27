385b0126269994ca14860f1c019233beafdea766
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-10-27 02:57:50 +0200

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import { Profile } from '../components/Task-01-Profile/Profile';
+import { Stats } from '../components/Task-02-Stats/Stats';
+import { FriendsList } from '../components/Task-03-Friends-List/FriendsList';
+import { TransactionHistory } from '../components/Task-04-Transaction-History/TransactionHistory';
+
+import user from '../components/Task-01-Profile/user.json';
+import data from '../components/Task-02-Stats/data.json';
+import friends from '../components/Task-03-Friends-List/friends.json';
+import transactions from '../components/Task-04-Transaction-History/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +20,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Stats stats={data} title="Upload stats" />
+      <FriendsList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+      React homework template!
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import user from './components/Task-01-Profile/user.json';
 
 import { App } from 'components/App';
-import { Profile } from './components/Task-01-Profile/Profile';
 
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
   </React.StrictMode>
 );
